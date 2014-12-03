902b9eaa7ed641020ddedb8aef8b18acd507a25c
Guangchuang Yu <guangchuangyu@gmail.com>
2014-12-03 23:29:00 +0800

    add getRoot, isRoot and getParent functions

---
+++ R/utilities.R
@@ -0,0 +1,33 @@
+getParent <- function(tr, node) {
+    if ( node == getRoot(tr) )
+        return(0)
+    
+    parent <- edge[,1]
+    child <- edge[,2]
+    res <- parent[child == node]
+    if (length(res) == 0) {
+        stop("cannot found parent node...")
+    }
+    if (length(res) > 1) {
+        stop("multiple parent found...")
+    }
+    return(res)
+}
+
+isRoot <- function(tr, node) {
+    getRoot(tr) == node
+}
+
+getRoot <- function(tr) {
+    edge <- tr$edge
+    ## 1st col is parent,
+    ## 2nd col is child,
+    parent <- unique(edge[,1])
+    child <- unique(edge[,2])
+    ## the node that has no parent should be the root
+    root <- parent[ ! parent %in% child ]
+    if (length(root) > 1) {
+        stop("multiple roots founded...")
+    }
+    return(root)
+}
