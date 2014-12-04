a5e206eae434e40ff57af282b424df650b5964fb
Guangchuang Yu <guangchuangyu@gmail.com>
2014-12-04 23:56:19 +0800

    implement geom_tree, geom_tiplab, geom_tippoint, geom_aline and theme_blank <2014-12-04, Thu>

---
+++ R/ggtree.R
@@ -0,0 +1,107 @@
+##' drawing phylogenetic tree from phylo object
+##'
+##' 
+##' @title ggtree
+##' @param tr phylo object
+##' @param ... additional parameter
+##' @return tree
+##' @importFrom ggplot2 ggplot
+##' @importFrom ggplot2 xlab
+##' @importFrom ggplot2 ylab
+##' @export
+##' @author Yu Guangchuang
+ggtree <- function(tr, ...) {
+    x <- y <- NULL
+    ggplot(tr, aes(x, y)) + geom_tree(...) + xlab("") + ylab("") + theme_blank()
+}
+
+##' add tree layer
+##'
+##' 
+##' @title geom_tree
+##' @param ... additional parameter
+##' @return tree layer
+##' @importFrom ggplot2 geom_segment
+##' @importFrom ggplot2 aes
+##' @export
+##' @author Yu Guangchuang
+geom_tree <- function(...) {
+    x <- y <- parent <- NULL
+    geom_segment(aes(x=c(x[parent], x[parent]),
+                     xend=c(x, x[parent]),
+                     y=c(y, y[parent]),
+                     yend=c(y, y)),...) 
+}
+
+##' add tip label layer
+##'
+##' 
+##' @title geom_tiplab 
+##' @param align align tip lab or not, logical
+##' @param hjust horizontal adjustment
+##' @param size label size
+##' @param ... additional parameter
+##' @return tip label layer
+##' @importFrom ggplot2 geom_text
+##' @export
+##' @author Yu Guangchuang
+geom_tiplab <- function(align=FALSE, hjust=-.25, size=5, ...) {
+    x <- y <- label <- isTip <- NULL
+    if (align == TRUE) {
+        geom_text(aes(x=max(x), label=label), subset=.(isTip), hjust=hjust, size=size, ...)
+    } else {
+        geom_text(aes(label=label), subset=.(isTip), hjust=hjust, size=size, ...)
+    }
+}
+
+##' add horizontal align lines
+##'
+##' 
+##' @title geom_aline
+##' @param linetype line type
+##' @param ... additional parameter
+##' @return aline layer
+##' @export
+##' @author Yu Guangchuang
+geom_aline <- function(linetype="dashed", ...) {
+    x <- y <- isTip <- NULL
+    geom_segment(aes(x=ifelse(x==max(x), x, x*1.02), xend=max(x), yend=y), subset=.(isTip), linetype=linetype, ...)
+}
+
+##' add points layer of tips 
+##'
+##' 
+##' @title geom_tippoint 
+##' @param ... additional parameter
+##' @return tip point layer
+##' @importFrom ggplot2 geom_point
+##' @export
+##' @author Yu Guangchuang
+geom_tippoint <- function(...) {
+    isTip <- NULL
+    geom_point(subset=.(isTip), ...)
+}
+
+##' blank theme
+##'
+##' 
+##' @title theme_blank
+##' @importFrom ggplot2 theme_bw
+##' @importFrom ggplot2 theme
+##' @importFrom ggplot2 element_blank
+##' @importFrom ggplot2 %+replace%
+##' @export
+##' @return NULL 
+##' @author Yu Guangchuang
+theme_blank <- function() {
+    theme_bw() %+replace%
+    theme(axis.text.x=element_blank(),
+          axis.text.y=element_blank(),
+          legend.position="none",
+          panel.grid.minor=element_blank(),
+          panel.grid.major=element_blank(),
+          panel.background=element_blank(),
+          axis.ticks=element_blank(),
+          panel.border=element_blank())
+}
+
--- R/utilities.R
+++ R/utilities.R
@@ -1,7 +1,73 @@
+getXcoord <- function(tr) {
+    edge <- tr$edge
+    parent <- edge[,1]
+    child <- edge[,2]
+    root <- getRoot(tr)
+
+    len <- tr$edge.length
+
+    N <- getNodeNum(tr)
+    x <- numeric(N)
+    x[-root] <- NA  ## only root is set to 0
+    currentNode <- root
+    while(any(is.na(x))) {
+        idx <- which(parent %in% currentNode)
+        newNode <- child[idx]
+        x[newNode] <- x[parent[idx]]+len[idx]
+        currentNode <- newNode
+    }
+
+    return(x)
+}
+
+##' @importFrom magrittr %>%
+##' @importFrom magrittr equals
+getYcoord <- function(tr) {
+    Ntip <- length(tr[["tip.label"]])
+    N <- getNodeNum(tr)
+
+    edge <- tr[["edge"]]
+    parent <- edge[,1]
+    child <- edge[,2]
+   
+    y <- 1:N
+    y[y>Ntip] <- NA
+
+    currentNode <- 1:Ntip
+    while(any(is.na(y))) {
+        newNode <-
+            which(child %in% currentNode) %>% 
+                parent[.] %>%
+                    table %>% equals(2) %>%  ## has 2 children
+                        which %>%
+                            names %>% 
+                                as.numeric
+
+       y[newNode] <- sapply(newNode, function(i) {
+            child[parent == i] %>% y[.] %>% mean           
+        })
+
+        currentNode <- parent %in% newNode %>% child[.] %>%
+            `%in%`(currentNode, .) %>% `!` %>%
+                currentNode[.] %>% c(., newNode)
+    }
+    
+    return(y)
+}
+    
+
+getNodeNum <- function(tr) {
+    Ntip <- length(tr[["tip.label"]])
+    Nnode <- tr[["Nnode"]]
+    ## total nodes
+    N <- Ntip + Nnode ## 2 * Ntip - 1
+    return(N)
+}
+
 getParent <- function(tr, node) {
     if ( node == getRoot(tr) )
         return(0)
-    
+    edge <- tr[["edge"]]
     parent <- edge[,1]
     child <- edge[,2]
     res <- parent[child == node]
@@ -19,7 +85,7 @@ isRoot <- function(tr, node) {
 }
 
 getRoot <- function(tr) {
-    edge <- tr$edge
+    edge <- tr[["edge"]]
     ## 1st col is parent,
     ## 2nd col is child,
     parent <- unique(edge[,1])
@@ -30,4 +96,9 @@ getRoot <- function(tr) {
         stop("multiple roots founded...")
     }
     return(root)
+}
+
+## from plyr package
+. <- function (..., .env = parent.frame()) {
+    structure(as.list(match.call()[-1]), env = .env, class = "quoted")
 }
