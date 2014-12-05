68acc578d21921ef20ab95bb500d239f70fa01c4
Guangchuang Yu <guangchuangyu@gmail.com>
2014-12-05 15:59:25 +0800

    dist.legend parameter

--- R/ggtree.R
+++ R/ggtree.R
@@ -11,13 +11,14 @@
 ##' @importFrom ggplot2 annotate
 ##' @export
 ##' @author Yu Guangchuang
-ggtree <- function(tr, ...) {
+ggtree <- function(tr, dist.legend=FALSE,...) {
     d <- x <- y <- NULL
-    ggplot(tr, aes(x, y)) + geom_tree(...) +
-        geom_segment(aes(x=0, xend= d <<- roundDigit(mean(length, na.rm=T)),
-                         y=0, yend=0)) +
-            annotate(geom="text", x=d/2, y=-0.6, label=d, size=5) +
-                xlab("") + ylab("") + theme_tree()
+    p <- ggplot(tr, aes(x, y)) + geom_tree(...) + xlab("") + ylab("") + theme_tree()
+    if (dist.legend == TRUE) {
+        p <- p + geom_segment(aes(x=0, xend= d <<- roundDigit(mean(length, na.rm=T)), y=0, yend=0)) +
+            annotate(geom="text", x=d/2, y=-0.6, label=d, size=5)
+    }
+    return(p)
 }
 
 ##' add tree layer
