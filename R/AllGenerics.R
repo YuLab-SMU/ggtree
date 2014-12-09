a6f23ba7fff310d7397e0dcb11a5b262f778259b
Guangchuang Yu <guangchuangyu@gmail.com>
2014-12-09 18:28:35 +0800

    as.binary

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -1,47 +1,37 @@
-##' get.tree method generics
-##'
-##'
+##' @title as.binary
+##' @param tree phylo, object
+##' @param ... additional parameter
+##' @export
+as.binary <- function(tree, ...) {
+    UseMethod("as.binary")
+}
+
+
 ##' @docType methods
 ##' @name get.tree
 ##' @rdname get.tree-methods
 ##' @title get.tree method
-##' @return tree text
 ##' @export
-##' @author Guangchuang Yu \url{http://ygc.name}
 setGeneric("get.tree", function(object, ...) standardGeneric("get.tree"))
 
-##' get.treeinfo method generics
-##'
-##'
 ##' @docType methods
 ##' @name get.treeinfo
 ##' @rdname get.treeinfo-methods
 ##' @title get.treeinfo method
-##' @return data.frame
 ##' @export
-##' @author Guangchuang Yu \url{http://ygc.name}
 setGeneric("get.treeinfo", function(object, ladderize=TRUE, right=FALSE, ...) standardGeneric("get.treeinfo"))
 
 
-##' get.placements method generics
-##'
-##'
 ##' @docType methods
 ##' @name get.placements
 ##' @rdname get.placements-methods
 ##' @title get.placements method
-##' @return data.frame
 ##' @export
-##' @author GuangchuangYu \url{http://ygc.name}
 setGeneric("get.placements", function(object, by, ...) standardGeneric("get.placements"))
 
-##' "set.treeinfo<-" method generic
-##'
-##'
 ##' @docType methods
 ##' @name "set.treeinfo<-"
 ##' @rdname set.treeinfo-methods
 ##' @title "set.treeinfo<-" method
 ##' @export
-##' @author Guangchuang Yu \url{http://ygc.name}
 setGeneric("set.treeinfo<-", function(x, value) standardGeneric("set.treeinfo<-"))
---
+++ R/treeIO.R
@@ -0,0 +1,113 @@
+##' convert polytomy to binary tree
+##'
+##' as.binary method for \code{phylo} object
+##' @title as.binary
+##' @param tree phylo, object
+##' @param ... additional parameter
+##' @return binary tree
+##' @method as.binary phylo
+##' @importFrom ape is.binary.tree
+##' @export
+##' @author Guangchuang Yu \url{http://ygc.name}
+as.binary.phylo <- function(tree, ...) {
+    if(is.binary.tree(tree)) {
+        cat("The input tree is already binary...\n")
+        invisible(tree)
+    }
+    polyNode <- tree$edge[,1] %>% table %>% '>'(2) %>%
+        which %>% names %>% as.numeric
+
+    N <- getNodeNum(tree)
+    
+    ii <- 0
+
+    for (pn in polyNode) {
+        idx <- which(tree$edge[,1] == pn)
+        while(length(idx) >2) {
+            ii <- ii + 1
+            newNode <- N+ii
+            tree$edge[idx[-1],1] <- newNode
+            newEdge <- matrix(c(tree$edge[idx[1],1], newNode), ncol=2)
+            tree$edge <- rbind(tree$edge, newEdge)
+            idx <- idx[-1]
+        }
+    }
+        
+    tree$Nnode <- tree$Nnode+ii
+    tree$edge.length <- c(tree$edge.length, rep(0, ii))
+    return(tree)
+}
+
+
+##' @method fortify jplace
+##' @importFrom ape read.tree
+##' @export
+fortify.jplace <- function(model, data, ladderize=TRUE, right=FALSE, ...) {
+    get.treeinfo(model, ladderize, right, ...)
+}
+
+
+##' @title fortify
+##' @param model phylo object
+##' @param data not use here
+##' @param ladderize ladderize, logical
+##' @param right logical
+##' @param ... additional parameter
+##' @return data.frame
+##' @importFrom ape ladderize
+##' @importFrom ggplot2 fortify
+##' @method fortify phylo
+##' @export
+##' @author Yu Guangchuang
+fortify.phylo <- function(model, data, ladderize=TRUE, right=FALSE, ...) {
+    if (ladderize == TRUE) {
+        tree <- ladderize(model, right=right)
+    } else {
+        tree <- model
+    }
+    
+    df <- as.data.frame(tree)
+    idx <- is.na(df$parent)
+    df$parent[idx] <- df$node[idx]
+    rownames(df) <- df$node
+    return(df)
+}
+
+##' @title as.data.frame
+##' @param x phylo object
+##' @param ... additional parameter
+##' @return data.frame
+##' @method as.data.frame phylo
+##' @export
+##' @author Yu Guangchuang
+as.data.frame.phylo <- function(x, ...) {
+    tip.label <- x[["tip.label"]]
+    Ntip <- length(tip.label)
+    N <- getNodeNum(x)
+    
+    edge <- as.data.frame(x[["edge"]])
+    colnames(edge) <- c("parent", "node")
+    if (is.null(x$edge.length)) {
+        x$edge.length <- rep(1, nrow(edge))
+    }
+    
+    edge$length <- x$edge.length
+    
+    xpos <- getXcoord(x)
+    ypos <- getYcoord(x)
+    xypos <- data.frame(node=1:N, x=xpos, y=ypos)
+
+    res <- merge(edge, xypos, by.x="node", by.y="node", all.y=TRUE)
+    label <- rep(NA, N)
+    label[1:Ntip] <- tip.label
+    if ( !is.null(x$node.label) ) {
+        label[(Ntip+1):N] <- x$node.label
+    }
+    res$label <- label
+    isTip <- rep(FALSE, N)
+    isTip[1:Ntip] <- TRUE
+    res$isTip <- isTip
+
+    return(res)
+}
+
