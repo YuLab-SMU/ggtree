46caf0ef891cb26ec0b09e820bed10eaa15d557e
Guangchuang Yu <guangchuangyu@gmail.com>
2014-12-19 20:11:52 +0800

    add test of converting polytomy to binary tree

--- tests/testthat/testthat.R
+++ tests/testthat/testthat.R
@@ -9,3 +9,10 @@ p <- ggtree(tr)
 test_that("tree record number of binary tree", {
 	expect_equal(nrow(p$data), 2*n-1)
 })
+
+
+polytomy <- read.tree(text="((A, B, C), D);")
+bt <- as.binary(polytomy)
+test_that("convert polytomy to binary tree", {
+    expect_equal(is.binary.tree(bt), TRUE)
+})
