8dd0cff355d51f5b6ead683e181847cdcf17a7de
Guangchuang Yu <guangchuangyu@gmail.com>
2014-12-28 22:34:54 +0800

    paml

---
+++ R/paml.R
@@ -0,0 +1,93 @@
+
+
+setClass("paml",
+         representation = representation(
+
+             )
+         )
+
+
+readPhylo.paml <- function(rstfile) {
+    ## works fine with baseml and codeml
+    rst <- readLines(rstfile)
+    tr.idx <- grep("\\)[ 0-9]*;", rst)
+
+    tr1 <- read.tree(text=rst[tr.idx][1])
+    tr3 <- read.tree(text=rst[tr.idx][3])
+
+    edge.idx <- grep("\\d+\\.\\.\\d+", rst)
+
+    edge.idx <- edge.idx[edge.idx < tr.idx[3]]
+
+    nodeNum <- strsplit(rst[edge.idx], split="\\.\\.") %>%
+        unlist %>% strsplit(split="[[:space:]]") %>% unlist
+
+    nodeNum %<>% `[`(nzchar(.))
+
+    edge <- matrix(as.numeric(nodeNum), ncol=2, byrow = TRUE)
+
+    label=c(tr3$tip.label, tr3$node.label)
+    root <- getRoot(tr3)
+    label %<>% `[`(. != root)
+
+    node.length <- data.frame(label=label,
+                              length=tr1$edge.length)
+
+    node.length$node <- sub("_\\w+", "", node.length$label)
+    node.length$label %<>% sub("\\d+_", "", .)
+    
+    edge <- as.data.frame(edge)
+    colnames(edge) <- c("parent", "node")
+
+    treeinfo <- merge(edge, node.length, by.x="node", by.y="node")
+    edge2 <- treeinfo[, c("parent", "node")]
+    edge2 %<>% as.matrix
+    
+    Ntip <- length(tr3$tip.label)
+
+    phylo <- list(edge= cbind(as.numeric(treeinfo$parent),
+                      as.numeric(treeinfo$node)),
+              edge.length = treeinfo$length,
+              tip.label = treeinfo$label[1:Ntip],
+              Nnode = tr3$Nnode,
+              node.label = c(root, treeinfo$label[(Ntip+1):nrow(treeinfo)]))
+
+    class(phylo) <- "phylo"
+    phylo <- reorder.phylo(phylo, "cladewise")
+    
+    return(phylo)
+}
+
+getAncSeq.paml <- function(rstfile, by="Marginal") {
+    ## works fine with baseml and codeml
+    rst <- readLines(rstfile)
+
+    by <- match.arg(by, c("Marginal", "Joint"))
+    query <- paste(by, "reconstruction of ancestral sequences") 
+    idx <- grep(query, rst)
+    if(length(idx) == 0) {
+        ## in some paml setting, joint_ancseq are not available. 
+        return(NA)
+    }
+    si <- grep("reconstructed sequences", rst)
+    idx <- si[which.min(abs(si-idx))]
+
+    nl <- strsplit(rst[idx+2], split=" ") %>% unlist %<>% `[`(nzchar(.))
+    N <- as.numeric(nl[1])
+    seq.leng <- as.numeric(nl[2])
+
+    seqs <- rst[(idx+4):(idx+3+N)]
+
+    seq.name <- character(N)
+    res <- character(N)
+    for (i in 1:N) {
+        ss <- gsub(" ", "", seqs[i])
+        nn <- nchar(ss)
+        res[i] <- substring(ss, nn-seq.leng+1,nn)
+        seq.name[i] <- substring(ss, 1, nn-seq.leng)
+    }
+    seq.name <- sub("\\w+#", "", seq.name)
+    names(res) <- seq.name
+
+    return(res)
+}
