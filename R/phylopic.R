3ddec9f2efaa66c6e80be819a94bee20728850a9
Guangchuang Yu <guangchuangyu@gmail.com>
2015-02-10 12:11:04 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 1 commit.
    Commit information:
    
    Commit id: 5d32e6887092dbadba9dd2646d71778514343859
    
        geom_phylopic
    
    Committed by: GuangchuangYu
    Author Name: GuangchuangYu
    Commit date: 2015-02-10 20:10:38 +0800
    Author date: 2015-02-10 20:10:38 +0800
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ggtree@99314 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/phylopic.R
+++ R/phylopic.R
@@ -7,32 +7,44 @@
 ##' @param color color
 ##' @param alpha alpha
 ##' @return grob object
-##' @importFrom grDevices rgb
-##' @importFrom grDevices col2rgb
-##' @importFrom RCurl getURLContent
-##' @importFrom png readPNG
 ##' @importFrom grid rasterGrob
 ##' @export
 ##' @author Guangchuang Yu
-##' @references https://github.com/sckott/rphylopic/blob/master/R/add_phylopic.r
 get.phylopic <- function(id, size=512, color="black", alpha=1) {
-    size %<>% as.character %>% match.arg(c("64", "128", "256", "512", "1024"))
-    img <- paste0("http://phylopic.org/assets/images/submissions/", id, ".", size, ".png") %>%
-        getURLContent %>%
-            readPNG
-
-    color %<>% col2rgb
-    n <- length(img[,,1])
-    
-    matrix(ifelse(img[,,4] > 0,
-                  rgb(red   = rep(color[1,1], n),
-                      green = rep(color[2,1], n),
-                      blue  = rep(color[3,1], n),
-                      alpha = img[,,4] * 255 * alpha, maxColorValue = 255),
-                  rgb(red   = rep(1, n),
-                      green = rep(1, n),
-                      blue  = rep(1, n),
-                      alpha = img[,,4] * alpha)), 
-           nrow = nrow(img)) %>%
-               rasterGrob
+    download.phylopic(id, size, color, alpha) %>% rasterGrob
 }
+
+##' download phylopic
+##'
+##' @title download.phylopic
+##' @param id phyopic id
+##' @param size size of phylopic
+##' @param color color
+##' @param alpha alpha
+##' @return matrix
+##' @importFrom grDevices rgb
+##' @importFrom grDevices col2rgb
+##' @importFrom EBImage readImage
+##' @importFrom EBImage channel
+##' @export
+##' @author Guangchuang Yu
+download.phylopic <- function(id, size=512, color="black", alpha=1) {
+    size %<>% as.character %>%
+        match.arg(c("64", "128", "256", "512", "1024"))
+
+    imgurl <- paste0("http://phylopic.org/assets/images/submissions/", id, ".", size, ".png")
+    imgfile <- tempfile(fileext = ".png")
+    download.file(imgurl, imgfile, quiet = TRUE)
+    img <- readImage(imgfile)
+       
+    color <- col2rgb(color) / 255
+
+    img <- channel(img, 'rgb')
+    img[,,1] <- color[1]
+    img[,,2] <- color[2]
+    img[,,3] <- color[3]
+    img[,,4] <- img[,,4]*alpha
+    
+    return(img)
+}
+
