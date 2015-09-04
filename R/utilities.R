c74f3fe3d2b0ec092b31381bf9d72359a35ea059
GuangchuangYu <guangchuangyu@gmail.com>
2015-09-04 10:58:45 +0800

    update %add2%

--- R/utilities.R
+++ R/utilities.R
@@ -392,10 +392,11 @@ is.tree_attribute_ <- function(p, var) {
 }
 
 `%add2%` <- function(d1, d2) {
-    d2[,1] <- as.character(d2[,1])
     if ("node" %in% colnames(d2)) {
+        d2 <- d2[,-1] ## drop label column
         dd <- merge(d1, d2, by.x="node", by.y="node", all.x=TRUE)
     } else {
+        d2[,1] <- as.character(d2[,1])
         dd <- merge(d1, d2, by.x="label", by.y=1, all.x=TRUE)
     }
     dd <- dd[match(d1$node, dd$node),]
