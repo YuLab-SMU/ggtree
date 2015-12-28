8858250a383a3290b71642e318a852b749bf234c
GuangchuangYu <guangchuangyu@gmail.com>
2015-12-28 19:34:05 +0800

    tree manipulation vignette

--- R/ggtree.R
+++ R/ggtree.R
@@ -31,16 +31,17 @@
 ##' tr <- rtree(10)
 ##' ggtree(tr)
 ggtree <- function(tr,
-                   mapping = NULL,
-                   showDistance=FALSE,
-                   layout="rectangular",
-                   mrsd = NULL,
-                   as.Date = FALSE,
-                   yscale="none",
+                   mapping        = NULL,
+                   layout         = "rectangular",
+                   mrsd           = NULL,
+                   as.Date        = FALSE,
+                   yscale         = "none",
                    yscale_mapping = NULL,
-                   ladderize = TRUE, right=FALSE,
-                   branch.length="branch.length",
-                   ndigits = NULL, ...) {
+                   ladderize      = TRUE,
+                   right          = FALSE,
+                   branch.length  = "branch.length",
+                   ndigits        = NULL,
+                   ...) {
 
     layout %<>% match.arg(c("rectangular", "slanted", "fan", "circular", "radial", "unrooted"))
 
@@ -48,7 +49,6 @@ ggtree <- function(tr,
         branch.length = "TREE"
     }
     
-    d <- x <- y <- NULL
     if(yscale != "none") {
         ## for 2d tree
         layout <- "slanted"
@@ -63,9 +63,9 @@ ggtree <- function(tr,
         type <- "none"
     }
     if (is.null(mapping)) {
-        mapping <- aes(x, y)
+        mapping <- aes_(~x, ~y)
     } else {
-        mapping <- modifyList(aes(x, y), mapping)
+        mapping <- modifyList(aes_(~x, ~y), mapping)
     }
     p <- ggplot(tr, mapping=mapping,
                 layout        = layout,
@@ -78,7 +78,7 @@ ggtree <- function(tr,
                 branch.length = branch.length,
                 ndigits       = ndigits, ...)
     
-    p <- p + geom_tree(layout, ...) + xlab("") + ylab("") + theme_tree2()
+    p <- p + geom_tree(layout, ...) + xlab(NULL) + ylab(NULL) + theme_tree()
     
     if (type == "circular" || type == "radial") {
         p <- p + coord_polar(theta = "y")
@@ -87,9 +87,6 @@ ggtree <- function(tr,
         p <- p + scale_y_continuous(limits=c(0, max(p$data$y)))
     } 
     
-    if (showDistance == FALSE) {
-        p <- p + theme_tree()
-    }
     attr(p, "mrsd") <- mrsd
     attr(p, "param") <- list(layout        = layout,
                              yscale        = yscale,
@@ -144,85 +141,7 @@ geom_tree <- function(layout="rectangular", ...) {
 
 
 
-##' tree theme
-##'
-##' 
-##' @title theme_tree
-##' @param bgcolor background color
-##' @param fgcolor foreground color
-##' @param ... additional parameter
-##' @importFrom ggplot2 theme_bw
-##' @importFrom ggplot2 theme
-##' @importFrom ggplot2 element_blank
-##' @importFrom ggplot2 %+replace%
-##' @export
-##' @return updated ggplot object with new theme
-##' @author Yu Guangchuang
-##' @examples
-##' require(ape)
-##' tr <- rtree(10)
-##' ggtree(tr) + theme_tree()
-theme_tree <- function(bgcolor="white", fgcolor="black", ...) {
-    theme_tree2() %+replace%
-    theme(panel.background=element_rect(fill=bgcolor, colour=bgcolor),
-          axis.line.x = element_blank(),
-          axis.text.x = element_blank(),
-          axis.ticks.x = element_blank(),
-          ...)
-}
 
-##' tree2 theme
-##'
-##' 
-##' @title theme_tree2
-##' @param bgcolor background color
-##' @param fgcolor foreground color
-##' @param ... additional parameter
-##' @importFrom ggplot2 theme_bw
-##' @importFrom ggplot2 theme
-##' @importFrom ggplot2 element_blank
-##' @importFrom ggplot2 element_line
-##' @importFrom ggplot2 %+replace%
-##' @importFrom ggplot2 element_rect
-##' @export
-##' @return updated ggplot object with new theme
-##' @author Yu Guangchuang
-##' @examples
-##' require(ape)
-##' tr <- rtree(10)
-##' ggtree(tr) + theme_tree2()
-theme_tree2 <- function(bgcolor="white", fgcolor="black", ...) {
-    theme_bw() %+replace%
-    theme(legend.position="none",
-          panel.grid.minor=element_blank(),
-          panel.grid.major=element_blank(),
-          panel.background=element_rect(fill=bgcolor, colour=bgcolor),
-          panel.border=element_blank(),
-          axis.line=element_line(color=fgcolor),
-          axis.line.y=element_blank(),
-          axis.ticks.y=element_blank(),
-          axis.text.y=element_blank(),
-          ...)
-}
-
-##' transparent background theme
-##'
-##' 
-##' @title theme_transparent
-##' @param ... additional parameter to tweak the theme
-##' @return ggplot object
-##' @importFrom ggplot2 theme
-##' @importFrom ggplot2 element_rect
-##' @export
-##' @author Guangchuang Yu
-theme_transparent <- function(...) {
-    theme(panel.background = element_rect(
-              fill = "transparent",
-              colour = NA),
-          plot.background = element_rect(
-              fill = "transparent",
-              colour = NA), ...)
-}
 
 
 ##' scale clade
