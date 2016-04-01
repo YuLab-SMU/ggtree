0bd36b2a86768f895efb78b2a70838a75f1d33d6
Guangchuang Yu <guangchuangyu@gmail.com>
2016-04-01 08:12:07 +0000

    geom_range
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ggtree@115592 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/geom_range.R
@@ -0,0 +1,58 @@
+##' bar of range (HPD, range etc) to present uncertainty of evolutionary inference
+##'
+##' 
+##' @title geom_range
+##' @param range range, e.g. "height_0.95_HPD"
+##' @param ... additional parameter, e.g. color, size, alpha
+##' @return ggplot layer
+##' @importFrom ggplot2 aes_string
+##' @export
+##' @author Guangchuang Yu
+geom_range <- function(range="height_0.95_HPD", ...) {
+
+    stat = "range"
+    position = "identity"
+    show.legend = NA
+    na.rm = TRUE
+    inherit.aes = FALSE    
+
+    if (range == "height_0.95_HPD") {
+        branch.length = "height"
+    } else {
+        branch.length = "branch.length"
+    }
+    default_aes <- aes_(x=~x, y=~y)
+    mapping <- modifyList(default_aes, aes_string(branch.length=branch.length, label=range))
+        
+    layer(
+        stat = StatRange,
+        mapping = mapping,
+        data = NULL,
+        geom = GeomSegment,
+        position = position,
+        show.legend=show.legend,
+        inherit.aes = inherit.aes,
+        params = list(na.rm = na.rm, ...)
+    )
+    
+}
+
+StatRange <- ggproto("StatRange", Stat,
+                     compute_group = function(self, data, scales, params) {
+                         ## label is actually the range
+                         
+                         df <- data[!is.na(data[,"label"]),]
+                         rr <- gsub("\\[", "", df[,"label"])
+                         rr <- gsub("\\]", "", rr)
+                         rr2 <- strsplit(rr, split=',') %>% do.call('rbind', .)
+                         rr2 <- matrix(as.numeric(rr2), ncol=2, byrow=FALSE)
+                         rr2 <- rr2 + df$x - df$branch
+                         data.frame(x = rr2[,1],
+                                    xend = rr2[,2],
+                                    y = df$y,
+                                    yend = df$y)
+                     },
+                     required_aes = c("x", "y", "xend", "yend")
+                     )
+
+
