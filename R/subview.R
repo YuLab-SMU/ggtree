6478c43cc26f666bb809837c46dd91044b433332
Guangchuang Yu <guangchuangyu@gmail.com>
2016-09-01 03:51:25 +0000

    bug fixed in subview
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ggtree@120638 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/subview.R
+++ R/subview.R
@@ -35,6 +35,9 @@ subview <- function(mainview, subview, x, y, width=.1, height=.1) {
     } else {
         stop("subview should be a ggplot or grob object, or an image file...")
     }
+
+    width <- width/2
+    height <- height/2
     
     mainview + annotation_custom(
         sv,
