6133057c3b34a7fb43a227a22aac8f1ad33d89ee
guangchuang yu <guangchuangyu@gamil.com>
2016-10-31 12:10:22 +0800

    set_tree_xlim

--- R/facet_plot.R
+++ R/facet_plot.R
@@ -1,6 +1,6 @@
 ##' plot tree associated data in an additional panel
 ##'
-##' 
+##'
 ##' @title facet_plot
 ##' @param p tree view
 ##' @param panel panel name for plot of input data
@@ -27,3 +27,29 @@ add_panel <- function(p, panel) {
     p$data <- df
     p + facet_grid(.~panel, scales="free_x")
 }
+
+##' set x axis limits for Tree panel
+##'
+##'
+##' @title set_tree_xlim
+##' @param tree_view tree view
+##' @param xlim xlim, should be of length 2
+##' @return updated tree view
+##' @export
+##' @importFrom ggplot2 geom_blank
+##' @author guangchuang yu
+set_tree_xlim <- function(tree_view, xlim) {
+    if (length(xlim) != 2) {
+        stop('-> xlim should be of length 2...')
+    }
+    x <- tree_view$data$x
+    if (is.na(xlim[1])) {
+        xlim[1] <- min(x)
+    }
+    if (is.na(xlim[2])) {
+        xlim[2] <- max(x)
+    }
+    dummy <- data.frame(x=xlim, panel='Tree')
+    tree_view + geom_blank(aes(x=x), dummy, inherit.aes = FALSE)
+}
+
