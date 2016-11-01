aed6d1a09850f396073d4b5c57282ae5b14824c1
guangchuang yu <guangchuangyu@gamil.com>
2016-11-01 10:38:09 +0800

    xlim_expand

--- R/xlim.R
+++ R/xlim.R
@@ -7,21 +7,21 @@
 ##' @export
 ##' @author guangchuang yu
 xlim_tree <- function(xlim) {
-    expand_xlim(xlim, panel='Tree')
+    xlim_expand(xlim, panel='Tree')
 }
 
 
 ##' expand x axis limits for specific panel
 ##'
 ##'
-##' @title expand_xlim
+##' @title xlim_expand
 ##' @param xlim xlim
 ##' @param panel panel
 ##' @return updated tree view
 ##' @importFrom ggplot2 geom_blank
 ##' @export
 ##' @author guangchuang yu
-expand_xlim <- function(xlim, panel) {
+xlim_expand <- function(xlim, panel) {
     dummy <- data.frame(x=xlim, .panel=panel)
     geom_blank(aes_(x=~x), dummy, inherit.aes = FALSE)
 }
