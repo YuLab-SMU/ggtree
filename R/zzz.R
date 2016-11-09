36b87ad2fb221187f3eec9e4d99f2b17347b79fd
guangchuang yu <guangchuangyu@gamil.com>
2016-11-09 12:43:48 +0800

    .onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,12 @@
-.onAttach <- function(libname, pkgname) {
+.onLoad <- function(libname, pkgname) {
     pkgVersion <- packageDescription(pkgname, fields="Version")
     msg <- paste0(pkgname, " v", pkgVersion, "  ",
                   "For help: https://guangchuangyu.github.io/", pkgname, "\n\n")
 
-    citation <- paste0("If you use ", pkgnameg, " in published research, please cite:\n",
+    citation <- paste0("If you use ", pkgname, " in published research, please cite:\n",
                   "Guangchuang Yu, David Smith, Huachen Zhu, Yi Guan, Tommy Tsan-Yuk Lam. ",
                   "ggtree: an R package for visualization and annotation of phylogenetic trees with their covariates and other associated data. ",
                   "Methods in Ecology and Evolution 2016, doi:10.1111/2041-210X.12628")
+
     packageStartupMessage(paste0(msg, citation))
 }
