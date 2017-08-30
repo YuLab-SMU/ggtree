f25c1cdddf98036233c62d164be0a9c275a12cbd
guangchuang yu <guangchuangyu@gamil.com>
2017-08-30 14:34:31 +0800

    set_hilight_legend

--- R/experimental_function.R
+++ R/experimental_function.R
@@ -260,4 +260,18 @@ coplot <- function(tree1, tree2, hjust=0) {
 
 
 
-
+##' set legend for multiple geom_hilight layers
+##'
+##'
+##' @title set_hilight_legend
+##' @param p ggtree object
+##' @param color color vector
+##' @param label label vector
+##' @return updated ggtree object
+##' @export
+##' @author guangchuang yu
+set_hilight_legend <- function(p, color, label) {
+	d <- data.frame(color=color, clade=label, x=0, y=1)
+	p <- p + geom_rect(aes_(fill=~clade, xmin=~x, xmax=~x, ymin=~y, ymax=~y), data=d, inherit.aes=F) +
+		guides(fill=guide_legend(override.aes=list(fill=d$color)))
+}
