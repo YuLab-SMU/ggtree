2fa23352c6afe02797e5446e285f7a9667b7f6e6
Watal M. Iwasaki <heavy.watal@gmail.com>
2018-02-15 01:52:40 +0900

    Refactor layoutDaylight and its related functions
    - Replace loops with dplyr::mutate or purrr::map
    - Return consistent values from getChild.df and getParent.df:
      integer(0) instead of 0 for tips and root.

--- R/MRCA.R
+++ R/MRCA.R
@@ -36,33 +36,16 @@ getMRCA.df_internal <- function(data, node1, node2) {
     node1 <- which(node1 == data$label | node1 == data[, "node"])
     node2 <- which(node2 == data$label | node2 == data[, "node"])
 
-    anc1 <- get.ancestor.df(data, node1)
-    anc2 <- get.ancestor.df(data, node2)
+    anc1 <- getAncestor.df(data, node1)
+    anc2 <- getAncestor.df(data, node2)
 
-    if(is.null(anc1)){
-      print("Warning getMRCA.df_internal(): node1 is root")
-    } else if(is.null(anc2)){
-      print("Warning getMRCA.df_internal(): node2 is root")
+    if (length(anc1) == 0L) {
+      warning("getMRCA.df_internal(): node1 is root")
+    }
+    if (length(anc2) == 0L) {
+      warning("getMRCA.df_internal(): node2 is root")
     }
 
     # Return common ancestors.
     intersect(c(node1, anc1), c(node2, anc2))[1]
-}
-
-
-get.ancestor.df <- function(df, node) {
-    pp <- getParent.df(df, node)
-    pp <- pp[pp != 0]
-    if (length(pp) == 0) {
-      #stop("input node is root...")
-      cat("WARNING: input node ",node," is root.")
-      return(NULL) # root has no ancestor
-    }
-    i <- 1
-    while(i <= length(pp)) {
-        pp <- c(pp, getParent.df(df, pp[i]))
-        pp <- pp[pp!=0]
-        i <- i+1
-    }
-    return(pp)
 }
