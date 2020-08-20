7601723f52fa69239cbdd6a965ab46626906f36a
xiayh17 <xiayh17@gmail.com>
2020-08-20 12:17:13 +0800

    add aesthetics section, exmple and references of geom_rootedge()

--- R/geom_rootedge.R
+++ R/geom_rootedge.R
@@ -1,12 +1,33 @@
-##' display root edge
-##'
-##'
-##' @title geom_rootedge
-##' @param rootedge length of rootedge; use phylo$root.edge if rootedge = NULL (by default).
-##' @param ... additional parameters
-##' @return ggplot layer
-##' @export
-##' @author Guangchuang Yu
+#' display root edge
+#'
+#' `geom_rootedge` supports number as input. And aesthetics of layer can be mapped.
+#' you can see the Aesthetics sections to set parameters.
+#' 
+#' @title geom_rootedge
+#' @param rootedge length of rootedge; use phylo$root.edge if rootedge = NULL (by default).
+#' @param ... additional parameters
+#' @section Aesthetics:
+#' \code{geom_rootedge()} understands the following  aesthethics (required aesthetics  are in bold):
+#'     \itemize{
+#'         \item \strong{\code{rootedge}} length of rootedge, default is NULL.
+#'         \item \code{size} control the width of rootedge, default is 0.5.
+#'         \item \code{colour} color of rootedge, default is black.
+#'         \item \code{linetype} the type of line, default is 1.
+#'         \item \code{alpha} modify colour transparency, default is 1.
+#'     }
+#' @return ggplot layer
+#' @export
+#' @author Guangchuang Yu
+#' @export 
+#' @examples 
+#' library(ggtree)
+#' set.seed(123)
+#' tr <- rtree(8)
+#' p <- ggtree(tr)
+#' ## length of rootedge is 1  
+#' p + geom_rootedge(rootedge = 1)
+#' ## a red rootedge 
+#' p +  geom_rootedge(rootedge = 1, color = "red")
 geom_rootedge <- function(rootedge = NULL, ...) {
     mapping <- aes_(x = ~x, y = ~y, xend = ~x, yend = ~y,
                     branch.length = ~branch.length,
