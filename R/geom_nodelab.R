e56c8b984d623fd1c236c352061eb1e9b3659f8f
xiangpin <xshuangbin@163.com>
2021-05-27 20:01:21 +0800

    remove match.call

--- R/geom_nodelab.R
+++ R/geom_nodelab.R
@@ -21,5 +21,7 @@ geom_nodelab <- function(mapping = NULL, nudge_x = 0, nudge_y = 0, geom = "text"
     #    mapping <- modifyList(self_mapping, mapping)
     #}
 
-    geom_tiplab(mapping, offset = nudge_x, nudge_y = nudge_y, geom = geom, hjust = hjust, ...)
+    p <- geom_tiplab(mapping, offset = nudge_x, nudge_y = nudge_y, geom = geom, hjust = hjust, ...)
+    p$nodelab <- TRUE
+    return (p)
 }
