4277af1060168b7af9bd26979fbdce21ece85c5a
Guangchuang Yu <guangchuangyu@gmail.com>
2021-10-28 10:09:46 +0800

    bioc

--- new_bioc_version.sh
+++ new_bioc_version.sh
@@ -2,10 +2,10 @@ make update;\
 sed -i 's/RELEASE_3_13/RELEASE_3_14/g' Makefile;\
 sed -i 's/RELEASE_3_12/RELEASE_3_13/g' Makefile;\
 
-make rmoldrelease;\
-make release;\
-git checkout master
-
 git add .;\
 git commit -m 'update bioc version';\
 git push
+
+make rmoldrelease;\
+make release;\
+git checkout master
