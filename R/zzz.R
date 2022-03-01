f1d8298d65c41761b409c0f0ce03d61192ca9429
xiangpin <xshuangbin@163.com>
2022-03-01 15:37:15 +0800

    update citation

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,15 @@
 ##' @importFrom utils packageDescription
+##' @importFrom pillar style_subtle
 .onAttach <- function(libname, pkgname) {
     pkgVersion <- packageDescription(pkgname, fields="Version")
-    msg <- paste0(pkgname, " v", pkgVersion, "  ",
-                  "For help: https://yulab-smu.top/treedata-book/", "\n\n")
+    msg <- style_subtle(paste0(pkgname, " v", pkgVersion, "  ",
+                  "For help: https://yulab-smu.top/treedata-book/", "\n\n"))
 
-    citation <- paste0("If you use ", pkgname,
-                       " in published research, please cite the most appropriate paper(s):\n\n",
-                       ggtree_citations(), "\n")
-
-    packageStartupMessage(paste0(msg, citation))
+    #citation <- paste0("If you use ", pkgname,
+    #                   " in published research, please cite the most appropriate paper(s):\n\n",
+    #                   ggtree_citations(), "\n")
+    citation <- random_ref()
+    packageStartupMessage(paste(strwrap(paste0(msg, citation)), collapse = "\n"))
 }
 
-
+random_ref <- getFromNamespace("random_ref", 'tidytree')
