a653972a27363440d122a02812f8f5ac3525c26a
xiangpin <xshuangbin@163.com>
2022-03-03 11:22:04 +0800

    update the print of refs

--- R/zzz.R
+++ R/zzz.R
@@ -1,15 +1,9 @@
 ##' @importFrom utils packageDescription
 ##' @importFrom pillar style_subtle
 .onAttach <- function(libname, pkgname) {
-    pkgVersion <- packageDescription(pkgname, fields="Version")
-    msg <- style_subtle(paste0(pkgname, " v", pkgVersion, "  ",
-                  "For help: https://yulab-smu.top/treedata-book/", "\n\n"))
-
-    #citation <- paste0("If you use ", pkgname,
-    #                   " in published research, please cite the most appropriate paper(s):\n\n",
-    #                   ggtree_citations(), "\n")
-    citation <- random_ref()
-    packageStartupMessage(paste(strwrap(paste0(msg, citation)), collapse = "\n"))
+    pkgVersion <- packageDescription(pkgname, fields = "Version")
+    ref <- random_ref(pkgname = pkgname, pkgVersion = pkgVersion, random_n = 2)
+    if (!is.null(ref)) packageStartupMessage(ref)
 }
 
 random_ref <- getFromNamespace("random_ref", 'tidytree')
