5281520b82578e0d0862fd41f856f8ceeb1b2350
xiangpin <xshuangbin@163.com>
2023-07-15 15:55:18 +0800

    add check.tbl_tree.verbose option

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,6 @@
 ##' @importFrom utils packageDescription
 .onAttach <- function(libname, pkgname) {
+    options(check.tbl_tree.verbose = FALSE)
     pkgVersion <- packageDescription(pkgname, fields = "Version")
     ref <- random_ref(pkgname = pkgname, pkgVersion = pkgVersion, random_n = 2)
     if (!is.null(ref)) packageStartupMessage(ref)
