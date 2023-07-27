208a06cbbfac643018e2c0ca6214491e4e40b76d
xiangpin <xshuangbin@163.com>
2023-07-27 21:14:25 +0800

    updata according to the function migrate of tidytree

--- R/treeio.R
+++ R/treeio.R
@@ -1,9 +1,8 @@
-has.slot <- treeio:::has.slot
-getNodeNum <- treeio:::getNodeNum
-getRoot <- tidytree:::rootnode
+#has.slot <- treeio:::has.slot
+#getNodeNum <- treeio:::getNodeNum
+getRoot <- tidytree::rootnode
 get.tree <- treeio::get.tree
-drop.tip <- treeio::drop.tip
-get.fields <- treeio::get.fields
+getNodeNum <- getFromNamespace('getNodeNum', 'treeio')
+has.slot <- getFromNamespace('has.slot', 'treeio')
 is.tree <- getFromNamespace('is.tree', 'treeio')
-
 set_branch_length <- getFromNamespace("set_branch_length", "treeio")
